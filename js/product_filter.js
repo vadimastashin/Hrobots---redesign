42e6e5a5696aaef3a5b880d5bc54c6109c9ff1cf
Вадим <vadastashin@yandex.ru>
2021-02-26 14:07:40 +0300

    Обновление разметки и контента

--- js/product_filter.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$("#btn-form").click(function(){
-		$(this).text($(this).text() == 'Показать' ? 'Скрыть' : 'Показать');
-		$("#rest-form").slideToggle();
-	});
-});	
--- js/tablespecs_product.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#btn-specs").click(function(){
-		$(this).text($(this).text() == 'Показать' ? 'Скрыть' : 'Показать');
-		$("#rest-table").slideToggle();
-	});
-});	
-
-
